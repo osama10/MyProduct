845d7b7b44b00dcd05e8fae5990cbfcd8396a341
Osama <osama.binbashir95@gmail.com>
2026-04-15 19:22:11 +0200

    Initial Commit

---
+++ MyProduct/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyProduct
+//
+//  Created by Osama Bin Bashir on 15.04.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyProduct/MyProductApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyProductApp.swift
+//  MyProduct
+//
+//  Created by Osama Bin Bashir on 15.04.26.
+//
+
+import SwiftUI
+
+@main
+struct MyProductApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
